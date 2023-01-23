2fb990a909d43c430948fc5d39dd23dd7d03263a
Deepak Kumar <48873989+deepakmp444@users.noreply.github.com>
2023-01-23 16:27:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,9 +24,8 @@ app.post("/message", async (req, res) => {
       expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
       secure: true,
       httpOnly: true,
-      sameSite: "Lax",
+      sameSite: "none",
       path: "/",
-      origin:"https://test1-frontend.vercel.app"
     };
 
     res
